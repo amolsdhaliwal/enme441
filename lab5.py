ebf454962d0e1f1d29e5449e54cbadd90efc8f88
Amol Dhaliwal <47928149+amolsdhaliwal@users.noreply.github.com>
2025-10-05 01:25:59 -0400

    Add files via upload

---
+++ lab5.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import math
+import time
+p = 4 # pick a gpio pin to put here
+GPIO.setmode(GPIO.BCM)
+GPIO.outout(p, GPIO.HIGH)
+pwm=GPIO.PWM(p, 500) # create 500hz pwm object
+
+try:
+    pwm.start(0) # start pwm with 0% duty cycle
+    while True:
+        t = time.time()
+        f=0.2 # hz
+        B = (math.sin(2 * math.pi * f * t))**2      
+        pwm.ChangeDutyCycle(B * 100) # change duty cycle  
+except KeyboardInterrupt:
+    pwm.stop()
+    GPIO.cleanup()
