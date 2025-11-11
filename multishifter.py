9b2a79241286d96ec589e3918c293f33a382a61c
Amol Dhaliwal <47928149+amolsdhaliwal@users.noreply.github.com>
2025-11-11 12:09:59 -0500

    Update multishifter.py

--- multishifter.py
+++ multishifter.py
@@ -13,7 +13,7 @@ class Stepper:
     seq = [0b0001, 0b0011, 0b0010, 0b0110,
            0b0100, 0b1100, 0b1000, 0b1001]
 
-    delay = 1200              # microseconds between steps
+    delay = 2500              # microseconds between steps
     steps_per_degree = 4096.0 / 360.0
 
     def __init__(self, shifter, lock, bit_start=None):
