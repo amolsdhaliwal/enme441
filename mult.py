e588d7d6b2d30dadfa75328394ba67d5ab1a5894
Amol Dhaliwal <47928149+amolsdhaliwal@users.noreply.github.com>
2025-11-12 12:27:33 -0500

    Update mult.py

--- mult.py
+++ mult.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
      #   m1.active_proc.join()
     #print("Actual angle:", m1.angle.value)
     #if m2.active_proc:
-        m2.active_proc.join()
+      #  m2.active_proc.join()
     #print("Actual angle:", m2.angle.value)
 
 
