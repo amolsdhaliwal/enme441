1a3365777928fa3614003ae4c14675c93795fc51
Amol Dhaliwal <47928149+amolsdhaliwal@users.noreply.github.com>
2025-11-12 11:23:51 -0500

    Update mult.py

--- mult.py
+++ mult.py
@@ -1,11 +1,7 @@
 import time
 import multiprocessing
 from shifter import Shifter   # your custom Shifter class
-import RPi.GPIO as GPIO
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)   # Use BCM numbering (matches your pin numbers)
-GPIO.cleanup()           # optional, clears any previous allocations
 
 
 class Stepper:
